a2512be0270859f08689cd2ff3db07047761c3ba
smeerung <gorpaimr@gmail.com>
2026-04-24 09:40:28 +0000

    c03

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,12 @@
+my_number = int(input("Enter a number: "))
+
+if my_number < 25:
+   while my_number < 25:
+        my_number += 1
+        print("Inside the loop, my variable is" , "" ,my_number)
+
+elif my_number > 25:
+    print("error")
+
+    
+    
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+ft = input("What you gotta say? ")
+wf = input("I got that! Anything else? ")
+while True:
+        print("I got that! Anything else? ")
+        
