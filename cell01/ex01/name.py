72c3cdf1df757a271f80a7c8b45fa7af761ab727
smeerung <gorpaimr@gmail.com>
2026-04-24 06:28:31 +0000

    c01 ex02

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -2,4 +2,5 @@ First_name = "pai"
 Last_name = "naja"
 whole_name = First_name + " " + Last_name
 
+
 print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,6 @@
+# Replace 25 with your actual current age
+pai_age = 17 
+taro_age = 42
+my_age = pai_age + taro_age
+
+print(my_age)
